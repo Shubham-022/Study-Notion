078c107854f270554e6a9c3ae7c8a34ab7d752bd
Shubham-022 <shubhambavistale859@gmail.com>
2026-02-16 15:34:47 +0530

    navbar error fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import { useState } from "react";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
